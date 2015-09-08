3536c10795e82dcbddc3c4b4402a16181ad3c6ce
tjangrot <tomek.jangrot@gmail.com>
2015-09-08 20:08:44 +0200

    1st cycle of TDD

---
+++ src/main/java/pl/jangrot/katas/berlinclock/BerlinClockConverter.java
@@ -0,0 +1,9 @@
+package pl.jangrot.katas.berlinclock;
+
+public class BerlinClockConverter implements TimeConverter {
+
+    @Override
+    public String convert(String time) {
+        return "";
+    }
+}
---
+++ src/main/java/pl/jangrot/katas/berlinclock/TimeConverter.java
@@ -0,0 +1,8 @@
+package pl.jangrot.katas.berlinclock;
+
+@FunctionalInterface
+public interface TimeConverter {
+
+    String convert(String time);
+
+}
