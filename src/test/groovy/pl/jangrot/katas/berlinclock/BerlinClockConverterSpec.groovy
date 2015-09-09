acb25f17ed6ae019fb218e8d08bde1de01199ad7
tjangrot <tomek.jangrot@gmail.com>
2015-09-09 12:32:10 +0200

    change the way of outputTime creation for "acceptance" tests

--- src/test/groovy/pl/jangrot/katas/berlinclock/BerlinClockConverterSpec.groovy
+++ src/test/groovy/pl/jangrot/katas/berlinclock/BerlinClockConverterSpec.groovy
@@ -9,9 +9,6 @@ import static org.assertj.core.api.Assertions.assertThat
 @Unroll
 class BerlinClockConverterSpec extends Specification {
 
-    @Shared
-    def newLine = System.lineSeparator()
-
     def "The BerlinClockConverter converts time"() {
         expect:
         assertThat(new BerlinClockConverter().convert("13:03:13")).isNotNull()
@@ -22,9 +19,9 @@ class BerlinClockConverterSpec extends Specification {
         new BerlinClockConverter().convert(inputTime) == outputTime
         where:
         inputTime  || outputTime
-        "00:00:00" || "Y" + newLine + "OOOO" + newLine + "OOOO" + newLine + "OOOOOOOOOOO" + newLine + "OOOO"
-        "13:17:01" || "O" + newLine + "RROO" + newLine + "RRRO" + newLine + "YYROOOOOOOO" + newLine + "YYOO"
-        "23:59:59" || "Y" + newLine + "RRRR" + newLine + "RRRO" + newLine + "YYRYYRYYRYY" + newLine + "YYYY"
-        "24:00:00" || "Y" + newLine + "RRRR" + newLine + "RRRR" + newLine + "OOOOOOOOOOO" + newLine + "OOOO"
+        "00:00:00" || String.format("Y%nOOOO%nOOOO%nOOOOOOOOOOO%nOOOO")
+        "13:17:01" || String.format("O%nRROO%nRRRO%nYYROOOOOOOO%nYYOO")
+        "23:59:59" || String.format("Y%nRRRR%nRRRO%nYYRYYRYYRYY%nYYYY")
+        "24:00:00" || String.format("Y%nRRRR%nRRRR%nOOOOOOOOOOO%nOOOO")
     }
 }
